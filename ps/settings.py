66f647e0164ab0ff6f5f5db0077257ee8341ace2
Sanjaikabilan <83118883+Sanjaikabilan@users.noreply.github.com>
2022-11-11 01:32:13 +0530

    fixed static

--- ps/settings.py
+++ ps/settings.py
@@ -121,11 +121,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
